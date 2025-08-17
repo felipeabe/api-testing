4eae79b37e600eb35ff7719910764742423e6e61
felipe <felipehabe301@gmail.com>
2025-08-17 09:48:17 -0300

    wire mock

--- src/test/java/felipeAbe/api_testing/ServiceConnectionConfig.java
+++ src/test/java/felipeAbe/api_testing/ServiceConnectionConfig.java
@@ -1,9 +1,12 @@
 package felipeAbe.api_testing;
 
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
 import java.util.List;
@@ -11,11 +14,17 @@ import java.util.List;
 @TestConfiguration(proxyBeanMethods = false)
 class ServiceConnectionConfig{
 
+
 	@Bean
 	@ServiceConnection
 	MySQLContainer<?> mysqlContainer() {
-		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+		var c=new MySQLContainer<>(DockerImageName.parse("mysql:9.1.0"))
+				.withUsername("admin")
+				.withPassword("123")
+				.withDatabaseName("promowisedb");
+		c.setPortBindings(List.of("3306:3306"));
 
+		return c;
 	}
 
 
--- src/test/java/felipeAbe/api_testing/TestApiTestingApplication.java
+++ src/test/java/felipeAbe/api_testing/TestApiTestingApplication.java
@@ -2,12 +2,18 @@ package felipeAbe.api_testing;
 
 import org.springframework.boot.SpringApplication;
 
+import static felipeAbe.api_testing.ContainerConfig.getProperties;
+import static felipeAbe.api_testing.ContainerConfig.wireMockContainer;
+
 
 public class TestApiTestingApplication {
 
 	public static void main(String[] args) {
+		wireMockContainer.start();
+		getProperties().forEach(System::setProperty);
 		SpringApplication.from(ApiTestingApplication::main)
-				.with(ServiceConnectionConfig.class).run(args);
+				.with(ServiceConnectionConfig.class)
+				.run(args);
 	}
 
 }
